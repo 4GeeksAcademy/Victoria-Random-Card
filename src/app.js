408ba6456a2b243246354beb52d203e411732111
Victoria González Rodríguez <159072629+Vickygonrod@users.noreply.github.com>
2024-04-08 17:57:30 +0000

    Random Card First Commit

--- src/app.js
+++ src/app.js
@@ -2,10 +2,43 @@
 import "bootstrap";
 import "./style.css";
 
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
+function randomCard() {
+  let palos = ["♦", "♥", "♠", "♣"];
+  let numeros = [
+    "A",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "J",
+    "Q",
+    "K"
+  ];
 
+  let palosIndex = Math.floor(Math.random() * palos.length);
+  let numerosIndex = Math.floor(Math.random() * numeros.length);
+
+  document.querySelector("#top").innerHTML = palos[palosIndex];
+  document.querySelector("#number").innerHTML = numeros[numerosIndex];
+  document.querySelector("#bottom").innerHTML = palos[palosIndex];
+
+  let colorPalos =
+    palos[palosIndex] == "♦" || palos[palosIndex] == "♥"
+      ? "text-danger"
+      : "text-dark";
+  document.querySelector("#top").className = colorPalos;
+  document.querySelector("#number").className = colorPalos;
+  document.querySelector("#bottom").className = colorPalos;
+}
+
+setInterval(() => randomCard(), 3000);
+
+document.querySelector("#btn").addEventListener("click", () => randomCard());
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  randomCard();
 };
